c56a876bfac6be962490c2346c22aebdeb5ad905
jonathanbain1992 <2092528b@student.gla.ac.uk>
2018-08-24 01:44:36 +0100

    initial commit.....

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jonathan/PycharmProjects/portfolio_django_actual/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
